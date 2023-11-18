73ceff1b2440cdb88bcd9e8ce3fe9de2bf8fd98c
anilates97 <anilates.97@gmail.com>
2023-11-19 01:43:04 +0300

    react slick library added to app to convert slider product object

--- frontend/src/components/Products/Products.jsx
+++ frontend/src/components/Products/Products.jsx
@@ -1,7 +1,53 @@
 import ProductItem from "./ProductItem";
 import "./Product.css";
+import { useState } from "react";
+import ProductsData from "../../data.json";
+import Slider from "react-slick";
+
+function NextBtn({ onClick }) {
+  return (
+    <button className="glide__arrow glide__arrow--right" onClick={onClick}>
+      <i className="bi bi-chevron-right"></i>
+    </button>
+  );
+}
+
+function PrevBtn({ onClick }) {
+  return (
+    <button className="glide__arrow glide__arrow--left" onClick={onClick}>
+      <i className="bi bi-chevron-left"></i>
+    </button>
+  );
+}
 
 function Products() {
+  const [products] = useState(ProductsData);
+
+  const sliderSettings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: <NextBtn />,
+    prevArrow: <PrevBtn />,
+    autoplaySpeed: 3000,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <section className="products">
       <div className="container">
@@ -10,19 +56,11 @@ function Products() {
           <p>Summer Collection New Morden Design</p>
         </div>
         <div className="product-wrapper product-carousel">
-          <div className="glide__track">
-            <ul className="product-list glide__slides" id="product-list">
-              <ProductItem />
-            </ul>
-          </div>
-          <div className="glide__arrows">
-            <button className="glide__arrow glide__arrow--left">
-              <i className="bi bi-chevron-left"></i>
-            </button>
-            <button className="glide__arrow glide__arrow--right">
-              <i className="bi bi-chevron-right"></i>
-            </button>
-          </div>
+          <Slider {...sliderSettings}>
+            {products.map((product) => (
+              <ProductItem product={product} key={product.id} />
+            ))}
+          </Slider>
         </div>
       </div>
     </section>
