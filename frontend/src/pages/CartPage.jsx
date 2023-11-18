98d17a0fbbe29335c8895cbfcffc1cadeeefbace
anilates97 <anilates.97@gmail.com>
2023-11-18 17:33:04 +0300

    all components with static data done

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -1,6 +1,5 @@
 import Cart from "../components/Cart/Cart";
 import Footer from "../components/Layout/Footer/Footer";
-import Header from "../components/Layout/Header/Header";
 
 function CartPage() {
   return <Cart />;
