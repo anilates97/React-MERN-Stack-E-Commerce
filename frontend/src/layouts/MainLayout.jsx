c195746dd666cf178c57f48546467de916fe55fd
anilates97 <anilates.97@gmail.com>
2023-11-18 21:56:46 +0300

    dialog modal added

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,13 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Footer from "../components/Layout/Footer/Footer";
 import Header from "../components/Layout/Header/Header";
 import Search from "../components/Modals/Search/Search";
+import Dialog from "../components/Modals/Dialog/Dialog";
 
 function MainLayout({ children }) {
   const [isSearchShow, setIsSearchShow] = useState(false);
+  const [isDialogShow, setIsDialogShow] = useState(false);
+
+  useEffect(() => {
+    const dialogStatus = localStorage.getItem("dialog")
+      ? JSON.parse(localStorage.getItem("dialog"))
+      : localStorage.setItem("dialog", JSON.stringify(true));
+    setTimeout(() => {
+      setIsDialogShow(dialogStatus);
+    }, 2000);
+  }, []);
 
   return (
     <div className="main-layout">
+      <Dialog isDialogShow={isDialogShow} setIsDialogShow={setIsDialogShow} />
       <Header setIsSearchShow={setIsSearchShow} />
       <Search isSearchShow={isSearchShow} setIsSearchShow={setIsSearchShow} />
       {children}
