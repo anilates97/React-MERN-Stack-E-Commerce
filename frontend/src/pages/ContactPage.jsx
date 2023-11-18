541364cb8a05d2dea9d3d590acb73466787f9cbf
anilates97 <anilates.97@gmail.com>
2023-11-18 15:16:43 +0300

    contact page completed

--- frontend/src/pages/ContactPage.jsx
+++ frontend/src/pages/ContactPage.jsx
@@ -1,5 +1,17 @@
+import Contact from "../components/Contact/Contact";
+import Footer from "../components/Layout/Footer/Footer";
+import Header from "../components/Layout/Header/Header";
+import Policy from "../components/Layout/Policy/Policy";
+
 function ContactPage() {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <Contact />
+      <Policy />
+      <Footer />
+    </>
+  );
 }
 
 export default ContactPage;
