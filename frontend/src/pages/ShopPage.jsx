9e886dc7b0ea342841af7e7fc1310165be5da365
anilates97 <anilates.97@gmail.com>
2023-11-18 15:08:15 +0300

    shoppage completed

--- frontend/src/pages/ShopPage.jsx
+++ frontend/src/pages/ShopPage.jsx
@@ -1,5 +1,22 @@
+import CampaignSingle from "../components/CampaignSingle/CampaignSingle";
+import Categories from "../components/Categories/Categories";
+import Footer from "../components/Layout/Footer/Footer";
+import Header from "../components/Layout/Header/Header";
+import Policy from "../components/Layout/Policy/Policy";
+import Products from "../components/Products/Products";
+
 function ShopPage() {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <Categories />
+      <Products />
+      <CampaignSingle />
+      <Products />
+      <Policy />
+      <Footer />
+    </>
+  );
 }
 
 export default ShopPage;
