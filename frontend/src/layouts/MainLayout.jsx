fe8e40e668a786b896e96c6f019e6164b529b1c8
anilates97 <anilates.97@gmail.com>
2023-11-18 21:36:45 +0300

    search modal added

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import Footer from "../components/Layout/Footer/Footer";
 import Header from "../components/Layout/Header/Header";
+import Search from "../components/Modals/Search/Search";
 
 function MainLayout({ children }) {
+  const [isSearchShow, setIsSearchShow] = useState(false);
+
   return (
-    <div>
-      <Header />
+    <div className="main-layout">
+      <Header setIsSearchShow={setIsSearchShow} />
+      <Search isSearchShow={isSearchShow} setIsSearchShow={setIsSearchShow} />
       {children}
       <Footer />
     </div>
