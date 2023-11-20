91e35462d76ce2a466dcbe5b39f7fed7e2808c1f
anilates97 <anilates.97@gmail.com>
2023-11-20 05:15:20 +0300

    created admin layout with ant design ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,11 +12,14 @@ import Header from "./components/Layout/Header/Header.jsx";
 import Footer from "./components/Layout/Footer/Footer.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BlogPage from "./pages/BlogPage.jsx";
+import AdminLayout from "./layouts/AdminLayout.jsx";
 function App() {
+  const isAdmin = window.location.pathname.startsWith("/admin");
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<MainLayout />}>
+        <Route element={isAdmin ? <AdminLayout /> : <MainLayout />}>
           {/* <Route index element={<Navigate replace to="/" />} /> */}
           <Route path="/" element={<HomePage />} />
           <Route path="shop" element={<ShopPage />} />
@@ -26,6 +29,7 @@ function App() {
           <Route path="auth" element={<AuthPage />} />
           <Route path="product/:id" element={<ProductDetailsPage />} />
           <Route path="blog/:id" element={<BlogDetailsPage />} />
+          <Route path="admin" element={<AdminLayout />} />
         </Route>
       </Routes>
     </BrowserRouter>
