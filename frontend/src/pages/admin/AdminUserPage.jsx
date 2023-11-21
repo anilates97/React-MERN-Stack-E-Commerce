bc039049f406713c3bc04c1123cb5a8740782511
anilates97 <anilates.97@gmail.com>
2023-11-21 04:18:52 +0300

    delete user in panel

--- frontend/src/pages/admin/AdminUserPage.jsx
+++ frontend/src/pages/admin/AdminUserPage.jsx
@@ -1,42 +1,63 @@
-import { Table } from "antd";
+import { Button, Popconfirm, Table, message } from "antd";
 import { useCallback, useEffect, useState } from "react";
-
-const columns = [
-  {
-    title: "Username",
-    dataIndex: "username",
-    key: "username",
-  },
-  {
-    title: "Email",
-    dataIndex: "email",
-    key: "email",
-  },
-  {
-    title: "Role",
-    dataIndex: "role",
-    key: "role",
-  },
-  {
-    title: "Avatar",
-    dataIndex: "avatar",
-    key: "avatar",
-    render: (imgSrc) => {
-      return (
-        <img
-          src={imgSrc}
-          alt="avatar"
-          style={{ width: "50px", borderRadius: "50%", height: "50px" }}
-        />
-      );
-    },
-  },
-];
 
 function AdminUserPage() {
   const apiUrl = import.meta.env.VITE_API_BASE_URL;
   const [dataSource, setDataSource] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  const columns = [
+    {
+      title: "Avatar",
+      dataIndex: "avatar",
+      key: "avatar",
+      render: (imgSrc) => {
+        return (
+          <img
+            src={imgSrc}
+            alt="avatar"
+            style={{ width: "50px", borderRadius: "50%", height: "50px" }}
+          />
+        );
+      },
+    },
+
+    {
+      title: "Username",
+      dataIndex: "username",
+      key: "username",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Role",
+      dataIndex: "role",
+      key: "role",
+    },
+    {
+      title: "Actions",
+      dataIndex: "actions",
+      key: "actions",
+      render: (_, record) => {
+        return (
+          <Popconfirm
+            title="Kullanıcıyı Sil"
+            description="Kullanıcıyı silmek istediğinizden emin misiniz?"
+            okText="Yes"
+            cancelText="No"
+            onConfirm={() => deleteUser(record.email)}
+          >
+            <Button type="primary" danger>
+              Delete
+            </Button>
+          </Popconfirm>
+        );
+      },
+    },
+  ];
 
   const fetchUsers = useCallback(async () => {
     setLoading(true);
@@ -46,13 +67,31 @@ function AdminUserPage() {
       if (response.ok) {
         const data = await response.json();
         setDataSource(data);
-      }
+      } else message.error("Veri getirme işlemi başarısız");
     } catch (err) {
-      console.log("Giriş hatası:", err);
+      console.log("Veri hatası:", err);
     } finally {
       setLoading(false);
     }
   }, [apiUrl]);
+
+  const deleteUser = async (userEmail) => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${apiUrl}/api/users/${userEmail}`, {
+        method: "DELETE",
+      });
+
+      if (response.ok) {
+        message.success("Kullanıcı başarıyla silindi");
+        fetchUsers();
+      } else message.error("Silme işlemi başarısız");
+    } catch (err) {
+      console.log("Silme hatası:", err);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
     fetchUsers();
